6716f84a2b2cdcf5fdab5a0fb68cac2b0b525327
veronicacheng2 <95119686+veronicacheng2@users.noreply.github.com>
2023-10-18 20:38:15 -0700

    Form Validation, Use of Copmuted Properties

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,5 +1,79 @@
 <template>
-    <div class="container mb-3">
+   <form action="" class="container mb-3">
+    <div class="row">
+     <div class="col-md-8">
+        <div class="mb-3">
+            <label for="" class="form-label">Page Title</label>
+            <input type="text" class="form-control" v-model="pageTitle"/>
+        </div>
+        <div class="mb-3">
+                <label for="" class="form-label">Content</label>
+                <textarea type="text" class="form-control" rows="5" v-model="content"/>
+        </div>
+     </div>
+     <div class="col">
+        <div class="mb-3">
+            <label for="" class="form-label">Link Text</label>
+            <input type="text" class="form-control" v-model="linkText">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Link URL</label>
+            <input type="text" class="form-control" v-model="linkUrl"/>
+        </div>
+        <div class="row mb-3">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox"/>
+                <label for="gridCheck1" class="form-check-label">Published</label>
+            </div>
+        </div>
+     </div>
+    </div>
+    <div class="mb-3">
+        <button class="btn btn-primary" @click.prevent="submitForm" :disabled="isFormInvalid">Create Page</button>
+    </div>
+   </form>
+</template>
+<script>
+export default {
+    props:['pageCreated'],
+    computed:{
+        isFormInvalid(){
+            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+        }
+    },
+    data(){
+        return {
+            pageTitle:"",
+            content:"",
+            linkText:"",
+            linkUrl:""
+        }
+    },
+    methods:{
+        submitForm(){
+            if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                alert("Please fill the form");
+                return;
+            }
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+                link:{
+                    text:this.linkText,
+                    url:this.linkUrl
+                }
+            })
+        },
+
+    }
+}
+
+
+/*
+<template>
+    
+    
+ <div class="container mb-3">
         <form>
             <div class="mb-3">
                 <label for="" class="form-label">Page Title</label>
@@ -15,16 +89,18 @@
                 <button class="btn btn-primary" @click.prevent="pageCreated({pageTitle,content})">Create Page</button>
             </div>
         </form>
-    </div>
+    </div>    
+    
+    
+    
+    
 </template>
-<script>
-export default {
-    props:['pageCreated'],
-    data(){
-        return {
-            pageTitle:"",
-            content:""
-        }
-    }
-}
+
+
+
+
+*/
+
+
 </script>
+
